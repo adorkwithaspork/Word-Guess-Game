a5dd37bfdcfaeef40909d0b2ed1d3e91f936a7cd
Grace Andrizzi <graceandrizzi@Graces-MacBook-Pro.local>
2018-07-31 22:08:19 -0700

    formatted html to bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,12 @@
 var words =["monkey", "elephant", "bird", "lion", "tiger"]
+    var word = words[Math.floor(Math.random() * words.length)];
+    console.log(word)
 
-var word = words[Math.floor(Math.random() * words.length)];
-console.log(word)
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+ answerArray[i] = "_";
+}
+ console.log(answerArray)
+ 
+ var remainingLetters = word.length;
+ console.log(remainingLetters)
